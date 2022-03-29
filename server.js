472d6f3f95a07d5dacc1f8415220d583be11d94c
Roopashree Mendon <menroo01@CSGICORP.COM>
2022-03-29 13:03:26 +0530

    done

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(`${__dirname}/infinite-scrolling-message-list/dist/`));
+app.use(express.static(__dirname + '/dist/Infinite-Scrolling-Message-List'));
 
-app.get('*', (req, res) => {
-    res.sendFile(`./infinite-scrolling-message-list/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname+ '/dist/Infinite-Scrolling-Message-List/index.html')); 
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
